5238bf0cad2af1ab1311e892d695532f091c0a09
fratur <francescoturco2795@gmail.com>
2025-07-03 20:13:38 +0200

    esercizio-6

---
+++ src/Esercizio6.java
@@ -0,0 +1,12 @@
+public class Esercizio6 {
+    public static void main(String[] args) {
+        
+        int [] numeri = {1, 2, 3, 4, 5};
+
+        for (int i = numeri.length - 1; i >= 0; i--) {
+            System.out.println(numeri[i]);
+
+        }
+
+    }
+}
